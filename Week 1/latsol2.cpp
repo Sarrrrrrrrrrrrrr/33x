d0d0b3c90798843694627f36f904cc2de7f7d0ef
Abisar <abisarfathir7@gmail.com>
2025-10-01 18:01:46 +0700

    Update Image

--- Week 1/latsol2.cpp
+++ Week 1/latsol2.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-
 using namespace std;
+int main (){
+    int r=20;
+    int s;
 
-int main(){
-    int r = 20;
-    int s; 
-    s = 20 + ++r;
-    cout << 'nilai r adalah : ' << r << endl;
-    cout << 'nilai s adalah : ' << s << endl;
+    s=20+ ++r;
+    cout << "Nilai r saat ini " << r << endl;
+    cout << "Nilai s saat ini " << s << endl;
     return 0;
 }
